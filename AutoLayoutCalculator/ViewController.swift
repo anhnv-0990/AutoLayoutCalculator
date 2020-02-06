2667c39eaf24cb29021afe6dba36df2599b406e7
anhnv-0990 <ngo.viet.anh@sun-asterisk.com>
2020-02-06 13:36:20 +0700

    Initial Commit

---
+++ AutoLayoutCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutCalculator
+//
+//  Created by Viet Anh on 2/6/20.
+//  Copyright © 2020 VietAnh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
